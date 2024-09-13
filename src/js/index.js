b325096eb55ad85f684d7b3e3f85b4d9341a1440
Anastasia <rotaru.nastia@gmail.com>
2024-09-13 22:53:14 +0300

    added canvas

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',() =>{
+    const canvas = document.getElementById('game-canvas')
+
+    // console.log({canvas})
+
+    canvas.width=1500;
+    canvas.height=500;
+
+    const ctx=canvas.getContext('2d');
+})
